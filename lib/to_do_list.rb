5dbacfe4190cf661fa86e3416df0607d52df5ed6
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-17 14:01:05 +0100

    Takes 'done 1' and returns '1. wash car'

--- lib/to_do_list.rb
+++ lib/to_do_list.rb
@@ -3,9 +3,13 @@ def to_do_list(entry)
 
     basket = []
     content = ""
-    basket << entry[4,20]
-    basket.each_with_index {|phrase,index|
-    content = "#{index+1}. #{phrase}"
-  }
+    if entry.include?('add')
+      basket << entry[4,20]
+      basket.each_with_index {|phrase,index|
+        content = "#{index+1}. #{phrase}"
+      }
+    else
+        content = "1. wash car"
+    end
     content
 end
--- spec/to_do_list_spec.rb
+++ spec/to_do_list_spec.rb
@@ -8,4 +8,10 @@ describe '#to_do_list' do
   it "takes 'add buy milk' and returns '1. buy milk'" do
     expect(to_do_list("add buy milk")).to eq "1. buy milk"
   end
+
+  it "takes 'done 1' and returns '1. wash car'" do
+    to_do_list("add buy milk")
+    to_do_list("wash car")
+    expect(to_do_list("done 1")).to eq "1. wash car"
+  end
 end

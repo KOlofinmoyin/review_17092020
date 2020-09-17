f055bfd2c3ca91152ce67049ddf5cd807952a36c
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-17 13:41:56 +0100

    Refactor.

--- lib/to_do_list.rb
+++ lib/to_do_list.rb
@@ -1,7 +1,11 @@
 def to_do_list(entry)
     return "please use the add or done keyword, followed by a to-do entry" if entry.nil? || entry == ""
 
-    if entry == "add buy milk"
-      "1. buy milk"
-    end
+    basket = []
+    content = ""
+    basket << entry[4,20]
+    basket.each_with_index {|phrase,index|
+    content = "#{index+1}. #{phrase}"
+  }
+    content
 end

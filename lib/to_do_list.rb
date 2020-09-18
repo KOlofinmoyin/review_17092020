4282533ff10f56eca8e3bc370b77c813bbc628ed
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-18 09:50:11 +0100

    Solution refactor with encapsulation.

--- lib/to_do_list.rb
+++ lib/to_do_list.rb
@@ -7,27 +7,38 @@ attr_reader :entry
   end
 
   def basket(entry)
-    return "please use the add or done keyword, followed by a to-do entry" if entry.nil? || entry == ""
+    return "Please use the add or done keyword, followed by a to-do entry" if entry.nil? || entry == ""
 
     if entry.include?('add')
-      @list_basket << entry[4,20]
-      @list_basket.each_with_index {|phrase,index|
-        @content = "#{index+1}. #{phrase}"
-      }
+      add_to_do_entry(entry)
       @content
     elsif entry.include?('done') && @list_basket.size == 1
+      delete_to_do_entry(entry)
       @content = "You don't have any current to-do list entries."
     elsif entry.include?('done')
-      outtake = entry.split(" ")
-      index = outtake.last.to_i
-      @list_basket.delete_at(index-1)
-      @list_basket.each_with_index {|phrase,index|
-        @content = "#{index+1}. #{phrase}"
-      }
+      delete_to_do_entry(entry)
       @content
     else
       @content = 0
     end
 
+  end
+
+  private
+
+  def add_to_do_entry(entry)
+    @list_basket << entry[4,20]
+    @list_basket.each_with_index {|phrase,index|
+      @content = "#{index+1}. #{phrase}"
+    }
+  end
+
+  def delete_to_do_entry(entry)
+    outtake = entry.split(" ")
+    index = outtake.last.to_i
+    @list_basket.delete_at(index-1)
+    @list_basket.each_with_index {|phrase,index|
+      @content = "#{index+1}. #{phrase}"
+    }
   end
 end
